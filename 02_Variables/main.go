6f809113e8b5b101fd006d10ed01dac08f796bcc
lukaskellerstein <lukas.kellerstein@gmail.com>
2017-12-28 13:11:35 +0100

    add README.md

--- 02_Variables/main.go
+++ 02_Variables/main.go
@@ -9,42 +9,53 @@ var b int
 var c, d, e int
 var f float32
 var g bool
+var h rune
 
 var za, zb, zc int = 1, 2, 3
 var ya, yb, yc = 1, 2, 3
 
-var aa = 5
+var aa = "some text"
+var bb = 5
+var hh = 'A'
 
 func main() {
 	//DECLARE
+	var aaa string
+	var bbb int
+
+	//DECLARE and INITIALIZE
+	var ccc = "some text"
+	var ddd = 555
+	eee := "some text"
+	fff := 555
 
 	//ASSIGN
 	a = "someString"
 	b = 112313213
 	c, d, e = 1, 2, 3
 
-	//INITIALIZATION
-
-	ab := 5
-
-	za = 1
-	zb = 2
-	zc = 3
-
-	ya = 1
-	yb = 2
-	yc = 3
-
+	//PRINT IT -----------------
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
 	fmt.Println(d)
 	fmt.Println(e)
-	fmt.Println(ab)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(h)
 	fmt.Println(za)
 	fmt.Println(zb)
 	fmt.Println(zc)
 	fmt.Println(ya)
 	fmt.Println(yb)
 	fmt.Println(yc)
+	fmt.Println(aa)
+	fmt.Println(bb)
+	fmt.Println(hh)
+	fmt.Println(aaa)
+	fmt.Println(bbb)
+	fmt.Println(ccc)
+	fmt.Println(ddd)
+	fmt.Println(eee)
+	fmt.Println(fff)
 }
